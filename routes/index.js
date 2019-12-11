832072277f46ac456fca25a8aff781b1f5261499
ST12 <ST12@ST12>
2019-12-11 12:16:02 +0300

    user route configuration

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const userRoutes = require('./user.routes');
 
 const router = express.Router();
+
+router.use('/users', userRoutes);
 
 router.get('/api-status', (req, res) =>
   res.json({
